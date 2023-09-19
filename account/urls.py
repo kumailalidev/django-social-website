199ed366e603a04bfea16747cdce861fc4425972
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-09-19 23:01:24 +0500

    Ch-04-Reset password views

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import include, path
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -6,18 +6,40 @@ urlpatterns = [
     # Previous login url
     # path("login/", views.user_login, name="login"),
     # Login / Logout urls
-    path("login/", auth_views.LoginView.as_view(), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    # path("login/", auth_views.LoginView.as_view(), name="login"),
+    # path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     # change password urls
-    path(
-        "password-change/",
-        auth_views.PasswordChangeView.as_view(),
-        name="password_change",
-    ),
-    path(
-        "password-change/done/",
-        auth_views.PasswordChangeDoneView.as_view(),
-        name="password_change_done",
-    ),
+    # path(
+    #     "password-change/",
+    #     auth_views.PasswordChangeView.as_view(),
+    #     name="password_change",
+    # ),
+    # path(
+    #     "password-change/done/",
+    #     auth_views.PasswordChangeDoneView.as_view(),
+    #     name="password_change_done",
+    # ),
+    # reset password urls
+    # path(
+    #     "password-reset/",
+    #     auth_views.PasswordResetView.as_view(),
+    #     name="password_reset",
+    # ),
+    # path(
+    #     "password-reset/done/",
+    #     auth_views.PasswordResetDoneView.as_view(),
+    #     name="password_reset_done",
+    # ),
+    # path(
+    #     "password-reset/<uidb64>/<token>/",
+    #     auth_views.PasswordResetConfirmView.as_view(),
+    #     name="password_reset_confirm",
+    # ),
+    # path(
+    #     "password-reset/complete/",
+    #     auth_views.PasswordResetCompleteView.as_view(),
+    #     name="password_reset_complete",
+    # ),
+    path("", include("django.contrib.auth.urls")),
     path("", views.dashboard, name="dashboard"),
 ]
